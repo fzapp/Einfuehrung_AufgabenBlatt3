0e772b56a9c1ac4d2903663df715e4c2ad8829af
fzapp <fzapp@smail.uni-loeln.de>
2025-11-05 12:24:32 +0100

    aenderung_Einfuehrung_Aufgabenblatt3_Aufg1

---
+++ src/p1/P1_main.java
@@ -0,0 +1,28 @@
+package p1;
+
+public class P1_main {
+
+	public static void main(String[] args) {
+		int wuerfel = 6;
+		
+		
+		if(wuerfel == 1) {
+			System.out.println("Sie haben gewonnen");
+			}
+		else if(wuerfel == 4) {
+			System.out.println("Sie haben gewonnen");
+		}
+		else if(wuerfel == 2) {
+			System.out.println("Sie haben verloren");
+		}
+		else if(wuerfel == 5) {
+			System.out.println("Es ist unentschieden");
+		}
+		else {
+			System.out.println("Das Ergebniss ist unbekannt");
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
