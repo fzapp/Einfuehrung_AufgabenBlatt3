38f34eb49c9b9f5981ab82cf83ef54ddb1fc3607
fzapp <fzapp@smail.uni-loeln.de>
2025-11-05 12:40:53 +0100

    aenderung_Einfuehrung_Aufgabenblatt3_Aufg2Ende&3

--- src/p2/P2_main.java
+++ src/p2/P2_main.java
@@ -4,6 +4,30 @@ public class P2_main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a = 2;
+		int b = 4;
+		int c = 1;
+		int d = -5;
+		
+		d = 0;
+		
+		if(a==b) { //E1
+			d++;
+		}
+		
+		if(a==b) {//E2
+			d++;
+		}
+		else if(a==c) {
+			d++;
+		}
+		
+		if(a+b>=c) {//E3
+			d++;
+		}
+		if(a+b+c>=100) {//E4
+			d++;
+		}
 
 	}
 
---
+++ src/p3/P3_main.java
@@ -0,0 +1,22 @@
+package p3;
+
+public class P3_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = -23;
+		
+		System.out.println(-i); //Aufgabe 1
+		
+		System.out.println(i*i); //Aufgabe 2
+		
+		if( i%2 == 0) { //Aufgabe 3
+			System.out.println(i + " ist gerade");
+		}
+		else {
+			System.out.println(i + " ist ungerade");
+		}
+
+	}
+
+}
