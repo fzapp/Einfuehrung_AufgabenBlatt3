50de0e0cea9e2c2f89546d16f573b85fffae0ee3
fzapp <fzapp@smail.uni-loeln.de>
2025-11-05 13:49:48 +0100

    aenderung_Einfuehrung_Aufgabenblatt3_HausAufg3

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -3,7 +3,22 @@ package h2;
 public class H2_main {
 
 	public static void main(String[] args) {
+		int jahr = 1345; boolean schalt = true;
+		
+		if (jahr % 400 == 0) {
+			schalt = true;
+		}
+		else if (jahr % 100 == 0) {
+			schalt = false;
+		}
+		else if (jahr % 4 == 0) {
+			schalt = true;
+		}
+		else {
+			schalt = false;
+		}
 		// TODO Auto-generated method stub
+		
 
 	}
 
